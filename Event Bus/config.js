8e7ff59d44dfc5464c92db6c8d46c3f61efae0de
Maciej Witkowski <maciekwitkowski322@gmail.com>
2021-09-07 12:42:53 +0200

    [Event Bus] Initial commit

---
+++ Event Bus/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	PORT: 4100,
+	// This adresses are subscribers
+	// In future this should be done by sending a request to /subscribe
+	URLS: [
+		"http://localhost:4000/events"
+	]
+}
