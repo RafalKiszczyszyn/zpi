02c01f7b2416091a48825db16b70a2082c28d2ab
Maciej Witkowski <maciekwitkowski322@gmail.com>
2021-09-22 12:44:35 +0200

    [Articles Api] Connection to the database

--- News API/src/config/index.ts
+++ News API/src/config/index.ts
@@ -1,3 +1,4 @@
 export default {
 	PORT: 4010,
+	DB: "mongodb+srv://backend:backend@cluster0.pifu7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
--- News API/src/index.ts
+++ News API/src/index.ts
@@ -1,14 +1,24 @@
 import express from 'express';
 import config from './config';
 import { ConsoleLogger } from './utils';
+import mongoose from 'mongoose';
 
 const app = express();
-const logger = ConsoleLogger("News API")
+const logger = ConsoleLogger("News API");
+
+// TODO: There might be a problem if the application starts but the database failed to connection
+// This situation can happen if either there is slow connection or database is down
+// Don't know how to fix it yet, but it can be fixed in the future.
+mongoose
+	.connect(config.DB)
+	.then(() => logger("Connection with database established successfully"))
+	.catch((err) => logger(err));
+
+mongoose.connection.on("error", (err) => logger(err));
+mongoose.connection.on("disconnected", () => logger("Connection with database closed"))
 
 app.post('/events', (req: express.Request, res: express.Response) => {
-
 	const event = req.body;
-
 	switch (event.type) {
 		case 'ArticleParsed':
 			const article = req.body.article;
@@ -19,10 +29,9 @@ app.post('/events', (req: express.Request, res: express.Response) => {
 			const payload = req.body.payload;
 			logger("Ping event consumed. Payload: " + payload);
 	}
-
 	res.sendStatus(200);
 });
 
 app.listen(config.PORT, () => {
-	logger(` Service started at http://localhost:${config.PORT}`);
+	logger(`Service started at http://localhost:${config.PORT}`);
 });
