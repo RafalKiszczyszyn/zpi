0e3d7b5ecc1a699728cb0d32e30321524b689347
Rafał Kiszczyszyn <rafalkiszczyszyn09@gmail.com>
2021-10-25 21:14:21 +0200

    Add RabbitMQ Dockerfile with SSL configuration

--- Event Bus/src/config/index.ts
+++
@@ -1,8 +0,0 @@
-export default {
-	PORT: 4100,
-	// This adresses are subscribers
-	// In future this should be done by sending a request to /subscribe
-	URLS: [
-		"http://localhost:4010/events" // News API
-	]
-}
--- Event Bus/src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Refactor
-export const ConsoleLogger = (serviceName: string) => {
-	return (message: string) => {
-		const date = new Date();
-		const timestamp = `[${date.toLocaleDateString("en")} ${date.toLocaleTimeString("pl")}]`;
-		// tslint:disable-next-line:no-console
-		console.log(`${timestamp} (${serviceName}) ${message}`);
-	}
-}
