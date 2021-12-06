59c4bf04254fcb94459267a1ee30ee0951c8a34b
RafalKiszczyszyn <66221734+RafalKiszczyszyn@users.noreply.github.com>
2021-12-06 12:59:38 +0100

    Add Python CI/CD Workflow (#9)
    * Add Github Action
    
    * Update python-tests.yml
    
    * Test Common
    
    * Test FeedReader
    
    * Test FeedReader 2
    
    * Fix Tools/PythonLibs/build.py
    
    * Fix python-tests.yml

--- Tools/PythonLibs/build.py
+++ Tools/PythonLibs/build.py
@@ -10,24 +10,25 @@ def main():
         print('[ERROR] Invalid arguments. Run install.py <source> <destination>')
         sys.exit(1)
 
-    source = sys.argv[1]
+    source = sys.argv[1] 
     destination = sys.argv[2]
     
+    cwd = os.getcwd()
     if not os.path.exists(source):
         print(f"[ERROR] Source='{source}' does not exist")
         sys.exit(1)
-
+    source = source if os.path.isabs(source) else os.path.join(cwd, source)
+        
     if not os.path.exists(destination):
         print(f"[ERROR] Destination='{destination}' does not exist")
         sys.exit(1)
+    destination = destination if os.path.isabs(destination) else os.path.join(cwd, destination)
 
     print('[INFO] Installing required dependencies')
     result = subprocess.run(['python', '-m', 'pip', 'install', 'wheel', 'setuptools'])
     if result.returncode != 0:
         print('[ERROR] Failed to install required dependencies')
         sys.exit(1)
-
-    cwd = os.getcwd()
     
     print("[INFO] Building wheel from setup.py")
     os.chdir(source)
@@ -38,8 +39,8 @@ def main():
 
     dist = os.path.join(source, 'dist')
     for path in filter(lambda file: file.endswith('.whl'), [path for path in os.listdir(dist)]):
-            print(f"[INFO] Coping wheel file from source='{dist}' to '{destination}'")
-            shutil.copy(os.path.join(dist, path), os.path.join(destination, path))
+        print(f"[INFO] Coping wheel file from source='{dist}' to '{destination}'")
+        shutil.copy(os.path.join(dist, path), os.path.join(destination, path))
 
     print('Done!')
     sys.exit(0)
