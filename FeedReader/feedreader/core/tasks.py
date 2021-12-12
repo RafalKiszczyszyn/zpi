a6da41f5d1d4a30e29820a6e76090b2e91e1aaaa
Rafał Kiszczyszyn <rafalkiszczyszyn09@gmail.com>
2021-12-12 17:31:01 +0100

    Fix Core package

--- FeedReader/feedreader/core/tasks.py
+++ FeedReader/feedreader/core/tasks.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import json
 from abc import abstractmethod, ABC
-from typing import List, Dict, Tuple, Any
+from typing import List, Tuple
 
 from feedreader.core import loading, config, exceptions
 
